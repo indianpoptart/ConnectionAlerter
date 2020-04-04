c7d19cbf83d4e83df5f7eed3aa4ec8e60bf98ce8
Nikhil Paranjape <admin@nikhilp.org>
2020-04-04 18:12:11 -0400

    New beginnings
    Some stuff has begun

---
+++ ConnectionAlerter/ConnectionAlerter/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ConnectionAlerter
+//
+//  Created by Nikhil Paranjape on 4/3/20.
+//  Copyright © 2020 Nikhil Paranjape. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
